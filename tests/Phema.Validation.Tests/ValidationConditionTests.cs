5de091dcfc5bddf79a2525400bf27497944843fd
Sergey <sergeyshaykhullin@gmail.com>
2019-02-02 00:32:17 +0300

    2.0.0 Validation tests part

--- tests/Phema.Validation.Tests/ValidationConditionTests.cs
+++ tests/Phema.Validation.Tests/ValidationConditionTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -96,6 +97,391 @@ namespace Phema.Validation.Tests
 				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
 			
 			Assert.Equal(3, raises);
+		}
+		
+		[Fact]
+		public void AddReturnsSameErrorAsContext()
+		{
+			var error1 = validationContext.When("key", 10)
+				.Is(value => true)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			var error2 = Assert.Single(validationContext.Errors);
+
+			Assert.Same(error1, error2);
+			Assert.Equal(error1, error2);
+		}
+
+		[Fact]
+		public void IfIsConditionIsTrueAddsError()
+		{
+			var error = validationContext.When("key", 10)
+				.Is(value => true)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.Equal("key", error.Key);
+			Assert.Equal("template1", error.Message);
+		}
+
+		[Fact]
+		public void IfNoIsConditionAddsError()
+		{
+			var (key, message) = validationContext.When("key", 10)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.Equal("key", key);
+			Assert.Equal("template1", message);
+		}
+
+		[Fact]
+		public void IfAllIsConditionIsTrueAddsError()
+		{
+			var (key, message) = validationContext.When("key", 10)
+				.Is(value => true)
+				.Is(value => true)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.Equal("key", key);
+			Assert.Equal("template1", message);
+		}
+
+		[Fact]
+		public void IsConditionIsTrueNotAddsError()
+		{
+			validationContext.When("key", 10)
+				.Is(value => false)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.Empty(validationContext.Errors);
+		}
+
+		[Fact]
+		public void ValidationMessageWithParameters()
+		{
+			var error = validationContext.When("key", 10)
+				.Is(value => true)
+				.AddError<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 12, 13);
+
+			Assert.Equal("key", error.Key);
+			Assert.Equal("template: 12, 13", error.Message);
+		}
+
+		[Fact]
+		public void ThrowsSameExceptionInIsCondition()
+		{
+			Assert.Throws<Exception>(() =>
+				validationContext.When("key", 10)
+					.Is(value => throw new Exception())
+					.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1));
+		}
+
+		[Fact]
+		public void ThrowsSameExceptionInAdd()
+		{
+			Assert.Throws<Exception>(() =>
+				validationContext.When("key", 10)
+					.Is(value => true)
+					.AddError<TestModelValidationComponent>(c => throw new Exception()));
+		}
+		
+		[Fact]
+		public void AspNetErrorSeverity()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddError<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template1", error.Message);
+			Assert.Equal(ValidationSeverity.Error, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetErrorSeverity_OneParameter()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddError<TestModelValidationComponent, int>(c => c.TestModelTemplate3, 11);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11", error.Message);
+			Assert.Equal(ValidationSeverity.Error, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetErrorSeverity_TwoParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddError<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 11, 22);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22", error.Message);
+			Assert.Equal(ValidationSeverity.Error, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetErrorSeverity_ThreeParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddError<TestModelValidationComponent, int, int, int>(c => c.TestModelTemplate5, 11, 22, 33);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22, 33", error.Message);
+			Assert.Equal(ValidationSeverity.Error, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetWarningSeverity()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddWarning<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template1", error.Message);
+			Assert.Equal(ValidationSeverity.Warning, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetWarningSeverity_OneParameter()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddWarning<TestModelValidationComponent, int>(c => c.TestModelTemplate3, 11);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11", error.Message);
+			Assert.Equal(ValidationSeverity.Warning, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetWarningSeverity_TwoParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddWarning<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 11, 22);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22", error.Message);
+			Assert.Equal(ValidationSeverity.Warning, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetWarningSeverity_ThreeParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddWarning<TestModelValidationComponent, int, int, int>(c => c.TestModelTemplate5, 11, 22, 33);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22, 33", error.Message);
+			Assert.Equal(ValidationSeverity.Warning, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetInformationSeverity()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddInformation<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template1", error.Message);
+			Assert.Equal(ValidationSeverity.Information, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetInformationSeverity_OneParameter()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddInformation<TestModelValidationComponent, int>(c => c.TestModelTemplate3, 11);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11", error.Message);
+			Assert.Equal(ValidationSeverity.Information, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetInformationSeverity_TwoParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddInformation<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 11, 22);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22", error.Message);
+			Assert.Equal(ValidationSeverity.Information, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetInformationSeverity_ThreeParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddInformation<TestModelValidationComponent, int, int, int>(c => c.TestModelTemplate5, 11, 22, 33);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22, 33", error.Message);
+			Assert.Equal(ValidationSeverity.Information, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetDebugSeverity()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddDebug<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template1", error.Message);
+			Assert.Equal(ValidationSeverity.Debug, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetDebugSeverity_OneParameter()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddDebug<TestModelValidationComponent, int>(c => c.TestModelTemplate3, 11);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11", error.Message);
+			Assert.Equal(ValidationSeverity.Debug, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetDebugSeverity_TwoParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddDebug<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 11, 22);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22", error.Message);
+			Assert.Equal(ValidationSeverity.Debug, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetDebugSeverity_ThreeParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddDebug<TestModelValidationComponent, int, int, int>(c => c.TestModelTemplate5, 11, 22, 33);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22, 33", error.Message);
+			Assert.Equal(ValidationSeverity.Debug, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetTraceSeverity()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddTrace<TestModelValidationComponent>(c => c.TestModelTemplate1);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template1", error.Message);
+			Assert.Equal(ValidationSeverity.Trace, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetTraceSeverity_OneParameter()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddTrace<TestModelValidationComponent, int>(c => c.TestModelTemplate3, 11);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11", error.Message);
+			Assert.Equal(ValidationSeverity.Trace, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetTraceSeverity_TwoParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddTrace<TestModelValidationComponent, int, int>(c => c.TestModelTemplate4, 11, 22);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22", error.Message);
+			Assert.Equal(ValidationSeverity.Trace, error.Severity);
+		}
+
+		[Fact]
+		public void AspNetTraceSeverity_ThreeParameters()
+		{
+			var model = new TestModel();
+
+			var error = validationContext.When("string", model.String)
+				.Is(value => value == null)
+				.AddTrace<TestModelValidationComponent, int, int, int>(c => c.TestModelTemplate5, 11, 22, 33);
+
+			Assert.NotNull(error);
+			Assert.Equal("string", error.Key);
+			Assert.Equal("template: 11, 22, 33", error.Message);
+			Assert.Equal(ValidationSeverity.Trace, error.Severity);
 		}
 	}
 }
--- tests/Phema.Validation.Tests/ValidationContextTests.cs
+++ tests/Phema.Validation.Tests/ValidationContextTests.cs
@@ -1,7 +1,40 @@
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
 namespace Phema.Validation.Tests
 {
 	public class ValidationContextTests
 	{
+		[Fact]
+		public void EmptyValidationResolves()
+		{
+			var services = new ServiceCollection();
+
+			services.AddPhemaValidation();
+
+			var provider = services.BuildServiceProvider();
+
+			var validationContext = provider.GetRequiredService<IValidationContext>();
+
+			Assert.IsType<ValidationContext>(validationContext);
+			Assert.Equal(ValidationSeverity.Error, validationContext.Severity);
+			Assert.Equal(0, validationContext.Errors.Count);
+		}
 		
+		[Fact]
+		public void ConfigureValidationOptionsChangesSeverity()
+		{
+			var services = new ServiceCollection();
+
+			services.AddPhemaValidation();
+
+			services.Configure<ValidationOptions>(options => options.Severity = ValidationSeverity.Debug);
+
+			var provider = services.BuildServiceProvider();
+
+			var validationContext = provider.GetRequiredService<IValidationContext>();
+
+			Assert.Equal(ValidationSeverity.Debug, validationContext.Severity);
+		}
 	}
 }
