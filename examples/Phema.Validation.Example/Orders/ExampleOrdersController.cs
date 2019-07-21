921917d227543ed5d69d809399402b8acf1a1d0e
Sergey <sergeyshaykhullin@gmail.com>
2019-07-21 18:57:56 +0300

    3.0.2 Example comment

--- examples/Phema.Validation.Example/Orders/ExampleOrdersController.cs
+++ examples/Phema.Validation.Example/Orders/ExampleOrdersController.cs
@@ -12,6 +12,19 @@ namespace Phema.Validation.Example
 			this.validationContext = validationContext;
 		}
 
+		/*
+			Try to remove some fields and watch for validation messages
+			POST
+			{
+				"name": "Cookies",
+				"cost": 4,
+				"address": {
+					"city": "City",
+					"street": "Street",
+					"house": 12
+				}
+			}
+		*/
 		[HttpPost]
 		public IActionResult CreateOrder([FromBody] ExampleOrderModel model)
 		{
